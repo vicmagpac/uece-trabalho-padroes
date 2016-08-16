870e12c30005a91c247e2dbe115d4780a70c9ec7
Victor Magalhães <victor.magalhaes@esp.ce.gov.br>
2016-08-16 10:51:35 -0300

    Ajuste - singleton

--- 1-singleton/Aplicacao.java
+++ 1-singleton/Aplicacao.java
@@ -1,11 +1,10 @@
 
 public class Aplicacao {
 	public static void main(String args[]) {
-		Logger log1 = new Logger();
-		log1.setAtivo(true);
-		log1.log("PRIMEIRA MENSAGEM DE LOG");
+		Logger log = Logger.getInstance();
+		log.setAtivo(true);
+		log.log("PRIMEIRA MENSAGEM DE LOG");
 
-		Logger log2 = new Logger();
-		log2.log("SEGUNDA MENSAGEM DE LOG");
+		log.log("SEGUNDA MENSAGEM DE LOG");
 	}
 }
--- 1-singleton/Logger.java
+++ 1-singleton/Logger.java
@@ -1,8 +1,11 @@
 
 public class Logger {
+
+	private static Logger unicaInstancia;
+
 	private boolean ativo = false;
 
-	public Logger() {}
+	private Logger() {}
 
 	public boolean isAtivo() {
 		return this.ativo;
@@ -10,6 +13,14 @@ public class Logger {
 
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	public static Logger getInstance() {
+		if (unicaInstancia == null) {
+			unicaInstancia = new Logger();
+		}
+
+		return unicaInstancia;
 	}
 
 	public void log(String mensagem) {
